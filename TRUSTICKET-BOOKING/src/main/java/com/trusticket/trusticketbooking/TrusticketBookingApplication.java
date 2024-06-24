1f8fe1de2cf6e403ef53e65e83a67f29c4bb3ef7
Jaehwi-So <sjh9708@gmail.com>
2024-06-24 15:28:15 +0900

    init : 프로젝트 초기세팅

---
+++ TRUSTICKET-BOOKING/src/main/java/com/trusticket/trusticketbooking/TrusticketBookingApplication.java
@@ -0,0 +1,13 @@
+package com.trusticket.trusticketbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrusticketBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrusticketBookingApplication.class, args);
+	}
+
+}
